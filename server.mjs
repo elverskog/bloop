7875244818a26d589bf58ddc2acce074949d0436
Erik Elverskog <erik@elverskog.com>
2023-03-23 17:18:42 -0400

    WIP adding build script - commented out for now

--- server.mjs
+++ server.mjs
@@ -4,6 +4,8 @@ import fs from "fs";
 import fsExtra from "fs-extra";
 import path from "path";
 import {fileURLToPath} from "url";
+import {getAllPages} from "./src/utils/dir-utils.mjs";
+
 
 //for node set a base directory as full path
 //for browser we set it in ./src/components/main.js 
@@ -18,6 +20,36 @@ const PORT = 3000;
 //so if in prod mode we don't need to keep writing the files on each page serve
 fsExtra.emptyDirSync(`${__basedir}/dist/css`);
 fsExtra.emptyDirSync(`${__basedir}/dist/js`);
+
+//if on prod, here we run code to request every page
+//this is the actual (main) build process
+//this will create CSS and JS files for each page on run, 
+//so that said file creation will be skipped on request
+//TODO: need to decide if this should be run here or elsewhere
+//in this current location the files will be created right on server start
+//and the request handling defined below will skip writing any CSS and JS files, as they should already exist (again, if on prod) 
+function build() {
+  console.log("BUILD");
+
+  //get an array of paths to all valid pages
+  const allPages = getAllPages(`${__basedir}/src/pages`);
+  //loop through said pages/paths
+  allPages.forEach( pagePath => {
+    console.log("page path: ", pagePath);
+    //"fabricate" req.url, as that is all we use from the request (for now)
+    const req = { url: pagePath};
+    //pass the url/path to our page builder 
+    moduleOrPageCompiler({ req, res: null, __basedir });
+  });  
+}
+
+  //run build script if on prod
+  console.log("NODE_ENV: ", process.env.NODE_ENV);
+
+  if(process.env.NODE_ENV === "production") {
+    //build();
+  }
+
 
 const server = http.createServer(async (req, res) => {
 
---
+++ src/utils/dir-utils.mjs
@@ -0,0 +1,53 @@
+import fs from "fs";
+
+//get an array of all valid files paths inside dirPath
+//function will augment an existing array, if passed in
+export function runAllPages(dirPath, result) { 
+
+  const currentFiles = fs.readdirSync(dirPath);
+
+  //loop through the files found
+  currentFiles.forEach(async file => {
+    //if the file is a dir, call this function with that dir as dirPath
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      getAllPages(`${dirPath}/${file}`, result);
+    //if the file is an MJS file import and run it
+    //this will add it's "various parts" to the hopper
+    } else if (file.split(".")[ file.split(".").length - 1 ] === "mjs") {
+      //TODO if this function is needed, should I check the mime-type?
+      // with something like https://www.npmjs.com/package/mime-types?
+      const module = (await import(`${dirPath}/${file}`)).default;
+      module();
+    }
+  })
+
+  return result;
+
+}
+
+
+
+
+//get an array of all valid files paths inside dirPath
+//function will augment an existing array, if passed in
+export function getAllPages(dirPath, result) { 
+
+  result = result || [];
+  const currentFiles = fs.readdirSync(dirPath);
+
+  //loop through the files found
+  currentFiles.forEach(function(file) {
+    //if the file is a dir, call this function with that dir as dirPath
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      getAllPages(`${dirPath}/${file}`, result);
+    //if the file is an MJS file add it to results
+    } else if (file.split(".")[ file.split(".").length - 1 ] === "mjs") {
+      //TODO if this function is needed, should I check the mime-type?
+      // with something like https://www.npmjs.com/package/mime-types?
+      result.push(`${dirPath}/${file}`);
+    }
+  })
+
+  return result;
+
+}
